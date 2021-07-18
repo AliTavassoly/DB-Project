0799030976225f2a650600d08317492e72f4c836
kasrakhoshjoo <kasrakhoshjoo@gmail.com>
2021-07-18 20:16:41 +0430

    one procedure created

--- scripts/views.sql
+++ scripts/views.sql
@@ -18,27 +18,30 @@ END //
 
 DELIMITER ; */
 
-DELIMITER //
-
-CREATE PROCEDURE new_user(EMAIL_ADD_IN varchar(15), FIRST_NAME_IN varchar(10), 
+-- DELIMITER //
+DROP PROCEDURE IF EXISTS QueraEcoPlus.new_user;
+CREATE PROCEDURE QueraEcoPlus.new_user(EMAIL_ADD_IN varchar(15), FIRST_NAME_IN varchar(10), 
                             LAST_NAME_IN varchar(10), NATIONAL_ID_IN varchar(10))
 BEGIN
     DECLARE user_id INT;
     DECLARE view_name varchar(100);
-
+    DECLARE sttmnt varchar(100);
     SELECT AUTO_INCREMENT FROM INFORMATION_SCHEMA.TABLES 
             WHERE TABLE_SCHEMA = 'QueraEcoPlus'
             AND TABLE_NAME = 'USER' INTO user_id;
+    SET user_id = 1;
 
-    SET view_name = CONCAT('user_', user_id, '_view');
 
     INSERT INTO QueraEcoPlus.USER (EMAIL_ADD, FIRST_NAME, LAST_NAME, NATIONAL_ID)
         VALUES  (EMAIL_ADD_IN, FIRST_NAME_IN, LAST_NAME_IN, NATIONAL_ID_IN);
 
-    CREATE VIEW @view_name AS SELECT USER.USER_ID, EMAIL_ADD, FIRST_NAME, LAST_NAME, NATIONAL_ID
-        FROM USER;
+    SET @sttmnt = CONCAT('CREATE VIEW QueraEcoPlus.view_', user_id ,' AS SELECT USER.USER_ID, EMAIL_ADD, FIRST_NAME, LAST_NAME, NATIONAL_ID FROM USER');
+
+    PREPARE exectutable FROM @sttmnt;
+    EXECUTE exectutable;
+
 END;
 
-CALL new_user('EEDA', 'FDAN', 'LADN', '2400');
-
+CALL QueraEcoPlus.new_user('EEDA', 'FDAN', 'LADN', '2400');
+-- 
 -- user's contests
