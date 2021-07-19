0b15eedd2607f0ecdeee11c645ec5e184c2ca921
AliTavassoly <tavassolyali@gmail.com>
2021-07-19 19:00:54 +0430

    A bug on trigger fixed

---
+++ scripts/script.sql
@@ -0,0 +1,13 @@
+INSERT INTO COMPANY VALUES('c1', 'c_add', 'cname', 
+                                'cfile', NULL, NULL, NULL);
+
+INSERT INTO SKILLS VALUES('skill1', 'type1');
+
+INSERT INTO ADVERTISEMENT VALUES('c1', 'add1', '.com', 1400, FALSE, 
+                                        8, NULL, NULL);
+
+INSERT INTO CV_WORK_INFO_INTRESTS VALUES(1, 'skill1');
+
+INSERT INTO ADV_RELATIVE_SKILLS VALUES('c1', 'add1', 'skill1');
+
+SELECT * FROM NEW_ADD_NOTIF;
